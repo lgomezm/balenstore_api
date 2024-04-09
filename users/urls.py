ee605807fe1487ae925b25e9a695cf7f3d06c93a
Luis Gomez <lgomezm90@gmail.com>
2024-04-08 21:11:58 -0500

    Added /me endpoint

--- users/urls.py
+++ users/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 from rest_framework_simplejwt.views import TokenObtainPairView
 
-from users.views import CreateUserView
+from users.views import CreateUserView, MeView
 
 
 urlpatterns = [
     path("login", TokenObtainPairView.as_view(), name="login"),
+    path("me", MeView.as_view(), name="me"),
     path("signup", CreateUserView.as_view(), name="signup"),
 ]
--- users/views.py
+++ users/views.py
@@ -1,3 +1,4 @@
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework import status
@@ -15,3 +16,12 @@ class CreateUserView(APIView):
             serializer.create()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class MeView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        user = request.user
+        serializer = UserSerializer(user)
+        return Response(serializer.data)
